bb278bee8cf9060ab2c59a3222122018d4c81839
Celeste Webb <celestial.webb@gmail.com>
2021-01-06 23:14:06 +0000

    avator inputs added

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ let randomNum = () => {
     console.log(number);
 };
 
-randomNum();
 
 //object of arrays of name, creature, power
-
+avator_inputs = {
+    name: ["Theodora", "Andromeda", "Delphine", "Elora", "Trixabelle"],
+    creature: ["Unicorn", "Fairy", "Elf", "Meremaid", "Nymph"],
+    power: ["Telekinesis", "Pyrokinesis", "Teleportation", "Invisibility", "Super Strength"]
+};
 
 
 //function to randomly select name, creature and power and input it into a message and return
