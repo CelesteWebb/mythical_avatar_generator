bcc7745effd3fd0050ac0f441a0bab700da8cc8f
Celeste Webb <celestial.webb@gmail.com>
2021-01-06 23:29:24 +0000

    generator function added

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 //function to generate random number between 0 and 4 - working
 let randomNum = () => {
     var number = Math.floor(Math.random() * 5);
-    console.log(number);
+    return number;
 };
 
 
+
 //object of arrays of name, creature, power
-avator_inputs = {
+let avator_inputs = {
     name: ["Theodora", "Andromeda", "Delphine", "Elora", "Trixabelle"],
-    creature: ["Unicorn", "Fairy", "Elf", "Meremaid", "Nymph"],
+    creature: ["Unicorn", "Fairy", "Elf", "Mermaid", "Nymph"],
     power: ["Telekinesis", "Pyrokinesis", "Teleportation", "Invisibility", "Super Strength"]
 };
 
 
+
 //function to randomly select name, creature and power and input it into a message and return
+let avator_generator = () => {
+    let random_Name = avator_inputs.name[randomNum()];
+    let random_Creature = avator_inputs.creature[randomNum()];
+    let random_Power = avator_inputs.power[randomNum()];
+
+    console.log(`You are a mythical being from another realm and your name is ${random_Name}. You are a magical ${random_Creature} and your unique power is ${random_Power}. Now go and enjoy the magical realm!!`);
 
 
 
+}
 
+avator_generator();
