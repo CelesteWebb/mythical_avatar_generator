2429530458948c2d2929e886af3d8e41829492d6
Celeste Webb <celestial.webb@gmail.com>
2021-01-06 22:59:11 +0000

    script added

---
+++ script.js
@@ -0,0 +1,12 @@
+//function to generate random number between 1 and 5
+
+
+//object of arrays of name, creature, power
+
+
+
+//function to randomly select name, creature and power and input it into a message and return
+
+
+
+
